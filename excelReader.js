3de9a2ee9482b32cf2ad7c95c2493f335f538d46
Bikash <thapabikash48@gmail.com>
2019-10-10 23:51:29 +0530

    API for Android app

---
+++ excelReader.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+// Get The File From The Input
+    var url = "http://localhost/Excelapi/story.xls";
+    var toDisplay = document.getElementById('total_story');
+    var toModified = document.getElementById('modified');
+
+
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", url, true);
+    oReq.responseType = "arraybuffer";
+    // Ready The Event For When A File Gets Selected
+    oReq.onload = function(e) {
+       var arraybuffer = oReq.response;
+
+        /* convert data to binary string */
+        var data = new Uint8Array(arraybuffer);
+
+        var arr = new Array();
+        for (var i = 0; i != data.length; ++i) {
+            arr[i] = String.fromCharCode(data[i]);
+        }
+
+        var bstr = arr.join("");
+        var cfb = XLS.read(bstr, { type: 'binary' });
+        toDisplay.innerHTML = (cfb.Strings.Count)/3;
+        modified.innerHTML = cfb.Custprops.ModifiedDate.slice(0,10);
+
+
+        // cfb.SheetNames.forEach(function(sheetName, index) {
+        //     var sCSV = XLS.utils.make_csv(cfb.Sheets[sheetName]);   
+        //     var data = XLS.utils.sheet_to_json(cfb.Sheets[sheetName], {header:1});
+        //     console.log(data[0])   
+        // });
+    }
+    // Tell JS To Start Reading The File.. You could delay this if desired
+    oReq.send()
+});
+    
